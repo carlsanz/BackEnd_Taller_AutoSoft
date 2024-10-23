56b3cad4cc3e29bbee330aff62f7433139b5809f
kennybenitez <kennybenitez2013@gmail.com>
2024-10-23 12:02:23 -0600

    Se agregaron las funcionalidades de buscar,actualizar y eliminar al empleado

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-const { agregarUsuarioCompleto } = require('../controllers/usuarioController');
+const { agregarUsuarioCompleto,
+    buscarUsuario,
+    actualizarUsuario,
+    eliminarUsuario
+ } = require('../controllers/usuarioController');
 
-
+//para agregar a los usuarios 
 router.post('/', agregarUsuarioCompleto); 
+
+//buscar usuario
+router.get('/:identidad', buscarUsuario);
+
+//actualizar usuario
+router.put('/:identidad', actualizarUsuario);
+
+//eliminar usuario
+router.delete('/:identidad/:Email', eliminarUsuario);
+
+
 
 
 module.exports = router;
