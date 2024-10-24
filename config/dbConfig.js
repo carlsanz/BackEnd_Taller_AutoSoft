dde1fc3a7ca2e4fb1000864ba9ff43a802593d88
carlsanz <carlsanz_2013@yahoo.com>
2024-10-24 10:59:36 -0600

    configuracion de la base de datos

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,13 +2,8 @@ const sql = require('mssql');
 
 const dbConfig = {
     user: 'sa',
-<<<<<<< HEAD
-    password: '541',
-    server: 'Lesther', // Usa solo el nombre de la máquina
-=======
     password: 'Man7equ!lla',
     server: 'DESKTOP-P8RVH97', // Usa solo el nombre de la máquina
->>>>>>> b632d7fa125ea92b5649a1a94a3a4fa98b40d294
     database: 'TallerMecanico',
     options: {
     
