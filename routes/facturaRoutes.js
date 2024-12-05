17e900cc64dabecbbca008bfd644d14a86f94b68
kennybenitez <kennybenitez2013@gmail.com>
2024-12-05 09:00:05 -0600

    se agrego funcion de eliminar factura

--- routes/facturaRoutes.js
+++ routes/facturaRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { generarFactura, obtenerFacturasSiEsMecanico, obtenerFacturasSiEsAdministrador } = require('../controllers/facturaControllers');
+const { generarFactura, obtenerFacturasSiEsMecanico, obtenerFacturasSiEsAdministrador, eliminarFactura } = require('../controllers/facturaControllers');
 const router = express.Router();
 
 // Ruta para generar la factura
@@ -8,6 +8,8 @@ router.post('/generar/:Id_cita', generarFactura);
 router.get('/Mecanico/:Id_empleado', obtenerFacturasSiEsMecanico);
 
 router.get('/admin/:Id_empleado', obtenerFacturasSiEsAdministrador);
+
+router.delete('/eliminarFactura/:Id_cita', eliminarFactura)
 
 
 
